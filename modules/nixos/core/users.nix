b69eaa754da90f137ad4bae89f7d53a785ceed5d
Conor Knowles <dev@conorknowles.com>
2025-10-11 16:10:49 +0100

    Attempt to fix hm startup with home dir

--- modules/nixos/core/users.nix
+++ modules/nixos/core/users.nix
@@ -13,6 +13,8 @@
           isNormalUser = true;
           extraGroups = [ "wheel" ];
           hashedPassword = "$6$o/.qISAqaJ2DbFIA$sFd/J56LT46H4CVH6SYKVRIUmK.KaFeePMj0xdPYzCksDc9a1J1VAEQkGJ9jUarAV68GIriIyc94w7qvgHlIp1";
+          home = "/home/goose";
+          createHome = true;
         };
       };
     };
