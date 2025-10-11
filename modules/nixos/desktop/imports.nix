63358f58acacc2a3992fd43fc1e392cd3abd8130
Conor Knowles <dev@conorknowles.com>
2025-10-11 15:34:38 +0100

    Change display manager

--- modules/nixos/desktop/imports.nix
+++ modules/nixos/desktop/imports.nix
@@ -4,7 +4,6 @@
     android
     auto-login
     bluetooth
-    display-manager
     keyring
     plymouth
     sound
--- modules/nixos/desktop/wayland/hyprland.nix
+++ modules/nixos/desktop/wayland/hyprland.nix
@@ -1,10 +1,30 @@
 {
-  flake.modules.nixos.desktop = {
+  flake.modules.nixos.desktop =
+  { pkgs, lib, ... }: {
     programs.hyprland = {
       enable = true;
       withUWSM = true;
+      xwayland.enable = true;
     };
     programs.uwsm.enable = true;
     security.pam.services.hyprlock = { };
+    users = {
+      users.greeter.group = "greeter";
+      groups.greeter = { };
+    };
+
+    services.greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${lib.getExe pkgs.tuigreet} --remember --cmd ${lib.getExe pkgs.hyprland}";
+          user = "greeter";
+        };
+        initial_session = {
+          command = "${lib.getExe pkgs.hyprland}";
+          user = "goose";
+        };
+      };
+    };
   };
 }
