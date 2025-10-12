e65524a285cc62b46685d1308bc00b7b938b690b
Conor Knowles <dev@conorknowles.com>
2025-10-12 20:56:50 +0100

    formatting

--- modules/nixos/desktop/users.nix
+++ modules/nixos/desktop/users.nix
@@ -1,11 +1,11 @@
 {
   flake.modules.nixos.users = {
     users.users.goose = {
-          isNormalUser = true;
-          extraGroups = [ "wheel" ];
-          hashedPassword = "$6$o/.qISAqaJ2DbFIA$sFd/J56LT46H4CVH6SYKVRIUmK.KaFeePMj0xdPYzCksDc9a1J1VAEQkGJ9jUarAV68GIriIyc94w7qvgHlIp1";
-          home = "/home/goose";
-          createHome = true;
-        };
-      };
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      hashedPassword = "$6$o/.qISAqaJ2DbFIA$sFd/J56LT46H4CVH6SYKVRIUmK.KaFeePMj0xdPYzCksDc9a1J1VAEQkGJ9jUarAV68GIriIyc94w7qvgHlIp1";
+      home = "/home/goose";
+      createHome = true;
+    };
+  };
 }
