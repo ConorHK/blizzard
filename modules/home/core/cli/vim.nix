6237be4fc5dd393ab0572c30482861a8643a68c2
Conor Knowles <knoconor@amazon.com>
2025-10-13 16:26:22 +0000

    Make cnvim opt in for home and export home

--- modules/home/core/cli/vim.nix
+++ modules/home/core/cli/vim.nix
@@ -1,6 +1,9 @@
 {
-  flake.modules.homeManager.core = {
+  flake.modules.homeManager.cnvim = 
+    { inputs, pkgs, ... }:
+    {
     home = {
+      packages = [ inputs.cnvim.packages.${pkgs.system}.nightly ];
       shellAliases.vim = "nvim";
       sessionVariables.EDITOR = "nvim";
     };
