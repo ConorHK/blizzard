41b16849c4105b4f75c274ea9311c1bc275dd6ed
Conor Knowles <dev@conorknowles.com>
2025-10-13 19:01:11 +0100

    Move back to alduin

--- modules/theme/colorscheme.nix
+++ modules/theme/colorscheme.nix
@@ -1,11 +1,24 @@
 {
   flake.modules.nixos.desktop =
     { pkgs, ... }:
-    let
-      # https://github.com/tinted-theming/schemes/tree/spec-0.11/base16/
-      theme = "atelier-forest";
-    in
     {
-      stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/${theme}.yaml";
+      stylix.base16Scheme = {
+        base00 = "#1c1c1c";
+        base01 = "#262626";
+        base02 = "#626262";
+        base03 = "#878787";
+        base04 = "#dfdfaf";
+        base05 = "#dfdfaf";
+        base06 = "#dfdfaf";
+        base07 = "#dfdfaf";
+        base08 = "#af5f5f";
+        base09 = "#af875f";
+        base0A = "#af875f";
+        base0B = "#87875f";
+        base0C = "#87afaf";
+        base0D = "#878787";
+        base0E = "#af8787";
+        base0F = "#87afaf";
+      };
     };
 }
