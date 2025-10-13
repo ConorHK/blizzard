3ef35c8237f573ef18da59573075de130e4beb14
Conor Knowles <dev@conorknowles.com>
2025-10-13 19:27:49 +0100

    Remove detnix substituter

--- modules/nixos/core/nix/determinate.nix
+++ modules/nixos/core/nix/determinate.nix
@@ -5,9 +5,6 @@
 
     nix.settings = {
       eval-cores = 0;
-
-      substituters = [ "https://install.determinate.systems" ];
-      trusted-public-keys = [ "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM" ];
     };
   };
 }
