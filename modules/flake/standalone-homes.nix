b84e41c69f901264ca4f52027988a95bbe20a2f1
Conor Knowles <dev@conorknowles.com>
2025-10-13 19:01:17 +0100

    Formatting

--- modules/flake/standalone-homes.nix
+++ modules/flake/standalone-homes.nix
@@ -8,83 +8,85 @@ let
   inherit (lib) types mkOption;
 
   # Define the module that can be imported by downstream flakes
-  standaloneHomesModule = {
-    inputs,
-    lib,
-    config,
-    ...
-  }: {
-    options = {
-      homeConfigurations =
-        let
-          homeConfigurationType = types.submodule {
-            options = {
-              username = mkOption {
-                type = types.str;
-                default = "goose";
-              };
+  standaloneHomesModule =
+    {
+      inputs,
+      lib,
+      config,
+      ...
+    }:
+    {
+      options = {
+        homeConfigurations =
+          let
+            homeConfigurationType = types.submodule {
+              options = {
+                username = mkOption {
+                  type = types.str;
+                  default = "goose";
+                };
 
-              homeDirectory = mkOption {
-                type = types.str;
-                default = "/home/goose";
-              };
+                homeDirectory = mkOption {
+                  type = types.str;
+                  default = "/home/goose";
+                };
 
-              system = mkOption {
-                type = types.str;
-                default = "x86_64-linux";
-              };
+                system = mkOption {
+                  type = types.str;
+                  default = "x86_64-linux";
+                };
 
-              unstable = lib.mkOption {
-                type = types.bool;
-              };
+                unstable = lib.mkOption {
+                  type = types.bool;
+                };
 
-              stateVersion = lib.mkOption {
-                type = types.str;
-                default = "25.05";
-              };
+                stateVersion = lib.mkOption {
+                  type = types.str;
+                  default = "25.05";
+                };
 
-              allowUnfree = lib.mkOption {
-                type = types.bool;
-                default = true;
+                allowUnfree = lib.mkOption {
+                  type = types.bool;
+                  default = true;
+                };
               };
             };
+          in
+          mkOption {
+            type = types.attrsOf homeConfigurationType;
+            default = { };
           };
-        in
-        mkOption {
-          type = types.attrsOf homeConfigurationType;
-          default = { };
-        };
-    };
+      };
 
-    config = {
-      flake.homeConfigurations =
-        let
-          mkHome =
-            name: options:
-            let
-              nixpkgs' = if options.unstable then inputs.nixpkgs else inputs.nixpkgs-stable;
-            in
-            inputs.home-manager.lib.homeManagerConfiguration {
-              pkgs = import nixpkgs' {
-                inherit (options) system;
-                config.allowUnfree = options.allowUnfree;
+      config = {
+        flake.homeConfigurations =
+          let
+            mkHome =
+              name: options:
+              let
+                nixpkgs' = if options.unstable then inputs.nixpkgs else inputs.nixpkgs-stable;
+              in
+              inputs.home-manager.lib.homeManagerConfiguration {
+                pkgs = import nixpkgs' {
+                  inherit (options) system;
+                  config.allowUnfree = options.allowUnfree;
+                };
+                extraSpecialArgs.inputs = inputs;
+                modules = [
+                  {
+                    home = {
+                      inherit (options) username homeDirectory stateVersion;
+                    };
+                    nix.package = lib.mkDefault nixpkgs'.legacyPackages.${options.system}.nix;
+                  }
+                  config.flake.modules.homeManager.core
+                  (config.flake.modules.homeManager."homeConfigurations/${name}" or { })
+                ];
               };
-              extraSpecialArgs.inputs = inputs;
-              modules = [
-                {
-                  home = {
-                    inherit (options) username homeDirectory stateVersion;
-                  };
-                  nix.package = lib.mkDefault nixpkgs'.legacyPackages.${options.system}.nix;
-                }
-                config.flake.modules.homeManager.core
-                (config.flake.modules.homeManager."homeConfigurations/${name}" or { })
-              ];
-            };
-        in
-        lib.mapAttrs mkHome config.homeConfigurations;
+          in
+          lib.mapAttrs mkHome config.homeConfigurations;
+      };
     };
-  };
 in
 {
   # Use the module locally
