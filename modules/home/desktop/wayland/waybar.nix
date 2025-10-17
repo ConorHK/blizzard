4644b0849227b939689ac6942d1f414bb77a3b55
Conor Knowles <dev@conorknowles.com>
2025-10-18 00:39:15 +0100

    Add systemd monitor

--- modules/home/desktop/wayland/waybar.nix
+++ modules/home/desktop/wayland/waybar.nix
@@ -1,88 +1,98 @@
 {
-  flake.modules.homeManager.waybar = { lib, pkgs, ... }: {
+  flake.modules.homeManager.waybar =
+    { lib, pkgs, ... }:
+    {
 
-    wayland.windowManager.hyprland.settings.exec-once = [
-      "systemctl --user start waybar"
-    ];
+      wayland.windowManager.hyprland.settings.exec-once = [
+        "systemctl --user start waybar"
+      ];
 
-    programs.waybar = {
-      enable = true;
-      systemd.enable = true;
-      style = ''
-        *{
-          all: unset;
-        }
-      '';
-      settings.main = {
-        name = "sidebar";
-        layer = "top";
-        position = "left";
-        spacing = 5;
-        modules-left = [ "clock" ];
-        modules-center = [
-          "hyprland/workspaces"
-          "niri/workspaces"
-        ];
-        modules-right = [
-          "privacy"
-          "wireplumber"
-          "network"
-        ];
-        "hyprland/workspaces" = {
-          disable-scroll = true;
-          on-click = "activate";
-          format = "{icon}";
-          format-icons = {
-            active = "•";
-            default = "•";
-          };
-          persistent-workspaces = {
-            "1" = [ ];
-            "2" = [ ];
-            "3" = [ ];
-            "9" = [ ];
-          };
-          sort-by-number = true;
-        };
-        clock = {
-          format = "{:%H\n%M\n--\n%d\n%m}";
-        };
-        network = {
-          interval = 1;
-          format = "";
-          format-ethernet = "e:con";
-          format-wifi = "w:con";
-          format-disconnected = "n:dis";
-          tooltip-format-wifi = "{essid} ({signalStrength}%)";
-          tooltip-format-ethernet = "{ifname}";
-          tooltip-format-disconnected = "disconnected";
-        };
-        wireplumber = {
-          format = "v:{volume}%";
-          format-muted = "m:{volume}%";
-          max-volume = 100;
-          scroll-step = 5;
-          on-click = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
-          on-click-right = "${lib.getExe pkgs.pwvucontrol}";
-        };
-        battery = {
-          states = {
-            good = 70;
-            warning = 30;
-            critical = 15;
-          };
-          format = "b:{capacity}%";
-          format-charging = "c:{capacity}%";
-        };
-        privacy = {
-          modules = [
-            {
-              type = "screenshare";
-              tooltip = true;
-            }
+      programs.waybar = {
+        enable = true;
+        systemd.enable = true;
+        style = ''
+          *{
+            all: unset;
+          }
+        '';
+        settings.main = {
+          name = "sidebar";
+          layer = "top";
+          position = "left";
+          spacing = 5;
+          modules-left = [ "clock" ];
+          modules-center = [
+            "hyprland/workspaces"
+            "niri/workspaces"
           ];
+          modules-right = [
+            "privacy"
+            "systemd-failed-units"
+            "wireplumber"
+            "network"
+          ];
+          "hyprland/workspaces" = {
+            disable-scroll = true;
+            on-click = "activate";
+            format = "{icon}";
+            format-icons = {
+              active = "•";
+              default = "•";
+            };
+            persistent-workspaces = {
+              "1" = [ ];
+              "2" = [ ];
+              "3" = [ ];
+              "9" = [ ];
+            };
+            sort-by-number = true;
+          };
+          clock = {
+            format = "{:%H\n%M\n--\n%d\n%m}";
+          };
+          network = {
+            interval = 1;
+            format = "";
+            format-ethernet = "e:con";
+            format-wifi = "w:con";
+            format-disconnected = "n:dis";
+            tooltip-format-wifi = "{essid} ({signalStrength}%)";
+            tooltip-format-ethernet = "{ifname}";
+            tooltip-format-disconnected = "disconnected";
+          };
+          wireplumber = {
+            format = "v:{volume}%";
+            format-muted = "m:{volume}%";
+            max-volume = 100;
+            scroll-step = 5;
+            on-click = "wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+            on-click-right = "${lib.getExe pkgs.pwvucontrol}";
+          };
+          battery = {
+            states = {
+              good = 70;
+              warning = 30;
+              critical = 15;
+            };
+            format = "b:{capacity}%";
+            format-charging = "c:{capacity}%";
+          };
+          privacy = {
+            modules = [
+              {
+                type = "screenshare";
+                tooltip = true;
+              }
+            ];
+          };
+          systemd-failed-units = {
+            hide-on-ok = false;
+            format-ok = "SYSOK";
+            format = "SYSF:{nr_failed}";
+            system = true;
+            user = true;
+          };
         };
       };
     };
-  };
 }
