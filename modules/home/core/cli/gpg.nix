e953657e729f1841be1fc69ecc1666d8af4c5e6c
Conor Knowles <dev@conorknowles.com>
2025-10-21 22:53:11 +0100

    Add scdaemon config

--- modules/home/core/cli/gpg.nix
+++ modules/home/core/cli/gpg.nix
@@ -2,8 +2,14 @@
   flake.modules.homeManager.core =
     { pkgs, ... }:
     {
+      # https://github.com/drduh/yubikey-guide
       programs = {
-        gpg.enable = true;
+        gpg = {
+          enable = true;
+        scdaemonSettings = {
+          disable-ccid = true;
+        };
+        };
       };
       services.gpg-agent = {
         enable = true;
