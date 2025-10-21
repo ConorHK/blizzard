72d9f91253e4501b4ddd82cd11cef31651f6cdb6
Conor Knowles <dev@conorknowles.com>
2025-10-21 21:48:10 +0100

    Add switch-branch

--- modules/home/core/cli/git.nix
+++ modules/home/core/cli/git.nix
@@ -1,6 +1,6 @@
 {
   flake.modules.homeManager.core =
-    { lib, ... }:
+    { lib, pkgs, ... }:
     {
       programs = {
         difftastic = {
@@ -41,8 +41,38 @@
             receive.fsckObjects = true;
             transfer.fsckobjects = true;
 
-            # https://bernsteinbear.com/git
-            alias.recent = "! git branch --sort=-committerdate --format=\"%(committerdate:relative)%09%(refname:short)\" | head -10";
+            alias =
+              let
+                switch-branch = pkgs.writeScriptBin "switch-branch" ''
+                  #!${pkgs.bash}/bin/bash
+                  set -o errexit
+                  set -o nounset
+
+                  selected_branch_ref=$(git branch --all --format='%(refname)' --sort='committerdate' | fzf --reverse)
+                  selected_branch_ref_prefix=''${selected_branch_ref#*/}
+                  selected_branch_source=''${selected_branch_ref_prefix%%/*}
+
+                  if test "$selected_branch_source" = 'remotes'; then
+                    branch_with_remote=''${selected_branch_ref#refs/remotes/}
+                    branch_without_remote=''${branch_with_remote#*/}
+                    (
+                      set -x
+                      git switch "$branch_without_remote" || git switch -t "$branch_with_remote"
+                    )
+                  else
+                    branch_name=''${selected_branch_ref#refs/heads/}
+                    (
+                      set -x
+                      git switch "$branch_name"
+                    )
+                  fi
+                '';
+              in
+              {
+                # https://bernsteinbear.com/git
+                recent = "! git branch --sort=-committerdate --format=\"%(committerdate:relative)%09%(refname:short)\" | head -10";
+                switch-branch = "! ${lib.getExe switch-branch}";
+              };
           };
         };
       };
@@ -56,7 +86,6 @@
         gl = "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
         gd = "git -c diff.external=difft diff";
         grc = "git -c diff.external=difft show --ext-diff";
-
       };
     };
 }
