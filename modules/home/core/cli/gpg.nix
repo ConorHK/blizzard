7f702451684507d2e68860408b773dee46d09aaf
Conor Knowles <dev@conorknowles.com>
2025-10-21 22:11:54 +0100

    Add gpg

---
+++ modules/home/core/cli/gpg.nix
@@ -0,0 +1,13 @@
+{
+  flake.modules.homeManager.core = { pkgs, ... }: {
+    programs = {
+      gpg.enable = true;
+    };
+    services.gpg-agent = {
+      enable = true;
+      defaultCacheTtl = 1800;
+      enableSshSupport = true;
+      pinentry.package = pkgs.pinentry-curses;
+    };
+    };
+}
