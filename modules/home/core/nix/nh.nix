2b63c1fb57d581fd4397e4f7a4f5ed4934d7e8c3
Conor Knowles <dev@conorknowles.com>
2025-11-02 15:56:57 +0000

    Wrap nh to define boot label

--- modules/home/core/nix/nh.nix
+++ modules/home/core/nix/nh.nix
@@ -1,11 +1,27 @@
 {
   flake.modules.homeManager.nh =
-    { lib, config, ... }:
+    {
+      lib,
+      config,
+      pkgs,
+      ...
+    }:
+    let
+      nh-wrapped = pkgs.writeShellScriptBin "nh" ''
+        #!/bin/sh
+        cd ${config.programs.nh.flake}
+        today=$(date +%Y%m%d)
+        branch=$(${lib.getExe pkgs.git} branch 2>/dev/null | sed -n '/^\* / { s|^\* ||; p; }')
+        revision=$(${lib.getExe pkgs.git} rev-parse HEAD)
+        export NIXOS_LABEL_VERSION="$today.$branch-''${revision:0:7}"
+        exec ${pkgs.nh}/bin/nh "$@"
+      '';
+    in
     {
       programs.nh = {
         enable = true;
-
         flake = lib.mkDefault "${config.home.homeDirectory}/repositories/blizzard";
+        package = nh-wrapped;
       };
     };
 }
