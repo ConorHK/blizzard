566535e949555f67162dae7c7479412caba78a70
Conor Knowles <dev@conorknowles.com>
2025-11-02 15:58:32 +0000

    Revert "Wrap nh to define boot label"
    This reverts commit 2b63c1fb57d581fd4397e4f7a4f5ed4934d7e8c3.

--- modules/home/core/nix/nh.nix
+++ modules/home/core/nix/nh.nix
@@ -1,27 +1,11 @@
 {
   flake.modules.homeManager.nh =
-    {
-      lib,
-      config,
-      pkgs,
-      ...
-    }:
-    let
-      nh-wrapped = pkgs.writeShellScriptBin "nh" ''
-        #!/bin/sh
-        cd ${config.programs.nh.flake}
-        today=$(date +%Y%m%d)
-        branch=$(${lib.getExe pkgs.git} branch 2>/dev/null | sed -n '/^\* / { s|^\* ||; p; }')
-        revision=$(${lib.getExe pkgs.git} rev-parse HEAD)
-        export NIXOS_LABEL_VERSION="$today.$branch-''${revision:0:7}"
-        exec ${pkgs.nh}/bin/nh "$@"
-      '';
-    in
+    { lib, config, ... }:
     {
       programs.nh = {
         enable = true;
+
         flake = lib.mkDefault "${config.home.homeDirectory}/repositories/blizzard";
-        package = nh-wrapped;
       };
     };
 }
