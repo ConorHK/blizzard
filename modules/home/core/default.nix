4dbe4b452339896d44488896a01c4f55aef028d0
Conor Knowles <dev@conorknowles.com>
2025-12-27 11:22:23 +0000

    Add serve-here script

--- modules/home/core/default.nix
+++ modules/home/core/default.nix
@@ -1,10 +1,18 @@
 {
   flake.modules.homeManager.core =
-    { lib, ... }:
+    {
+      lib,
+      inputs,
+      pkgs,
+      ...
+    }:
     {
       home = {
         username = lib.mkDefault "goose";
         homeDirectory = lib.mkDefault "/home/goose";
       };
+      home.packages = [
+        inputs.self.packages.${pkgs.system}.serve-here
+      ];
     };
 }
---
+++ modules/packages/serve-here.nix
@@ -0,0 +1,33 @@
+{
+  perSystem =
+    { pkgs, ... }:
+    {
+      packages.serve-here =
+        let
+          uploadserver = pkgs.python3Packages.buildPythonPackage rec {
+            pname = "uploadserver";
+            version = "5.2.1";
+
+            src = pkgs.fetchPypi {
+              inherit pname version;
+              hash = "sha256-qp2xkzLvnrnx8dHZpwlF3RjRg8jYC7WAaVS4ltJFZaU=";
+            };
+
+            doCheck = false;
+            pyproject = true;
+            build-system = [ pkgs.python3Packages.setuptools ];
+          };
+        in
+        pkgs.writeShellScriptBin "serve-here" ''
+          set -e
+
+          PORT=''${1:-8000}
+
+          ${pkgs.iptables}/bin/iptables -I nixos-fw -p tcp --dport "$PORT" -j ACCEPT
+
+          trap '${pkgs.iptables}/bin/iptables -D nixos-fw -p tcp --dport "$PORT" -j ACCEPT 2>/dev/null || true' EXIT INT TERM
+
+          exec ${pkgs.python3.withPackages (ps: [ uploadserver ])}/bin/python -m uploadserver "''${@}"
+        '';
+    };
+}
