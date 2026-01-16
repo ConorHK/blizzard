b5c934135667254c82edc06b8bc14ac41c0bba26
Conor Knowles <knoconor@amazon.com>
2026-01-16 13:42:22 +0000

    chore: Formatting

--- modules/home/core/syncthing.nix
+++ modules/home/core/syncthing.nix
@@ -1,23 +1,25 @@
 {
-  flake.modules.homeManager.syncthing = { lib, ... }: {
-    services.syncthing = {
-      enable = lib.mkDefault true;
-      settings = {
-        # Add more in home files
-        devices = {
-          "phone".id = "XKQFBFW-377EUDV-HH72KPJ-HQMD2FT-3XCC6DE-6EDNETZ-YDQ4BI5-KZ7TLAW";
-          # temp
-          "server".id = "ACCG7I6-AV3WNQV-4IX26BM-7GL2KCR-W3PDMLC-PYTRVZI-2ROUULM-T4Y7IQD";
-        };
-        folders.share = {
-          path = "~/share";
-          devices = [
-            "phone"
-            "server"
-          ];
-        };
+  flake.modules.homeManager.syncthing =
+    { lib, ... }:
+    {
+      services.syncthing = {
+        enable = lib.mkDefault true;
+        settings = {
+          # Add more in home files
+          devices = {
+            "phone".id = "XKQFBFW-377EUDV-HH72KPJ-HQMD2FT-3XCC6DE-6EDNETZ-YDQ4BI5-KZ7TLAW";
+            # temp
+            "server".id = "ACCG7I6-AV3WNQV-4IX26BM-7GL2KCR-W3PDMLC-PYTRVZI-2ROUULM-T4Y7IQD";
+          };
+          folders.share = {
+            path = "~/share";
+            devices = [
+              "phone"
+              "server"
+            ];
+          };
 
+        };
       };
     };
-  };
 }
