9414b051a2098f589cc50a8d3ba9d4a2c48ee1c3
Conor Knowles <knoconor@amazon.com>
2026-03-04 13:56:33 +0000

    Add variant option to vim

--- modules/home/core/cli/vim.nix
+++ modules/home/core/cli/vim.nix
@@ -1,11 +1,29 @@
 {
   flake.modules.homeManager.cnvim =
-    { inputs, pkgs, ... }:
     {
-      home = {
-        packages = [ inputs.cnvim.packages.${pkgs.system}.nightly ];
-        shellAliases.vim = "nvim";
-        sessionVariables.EDITOR = "nvim";
+      inputs,
+      pkgs,
+      lib,
+      config,
+      ...
+    }:
+    let
+      cfg = config.programs.cnvim;
+      packages = inputs.cnvim.packages.${pkgs.system};
+    in
+    {
+      options.programs.cnvim.variant = lib.mkOption {
+        type = lib.types.str;
+        default = "nightly";
+        description = "Which cnvim package output to install";
+      };
+
+      config = {
+        home = {
+          packages = [ packages.${cfg.variant} ];
+          shellAliases.vim = "nvim";
+          sessionVariables.EDITOR = "nvim";
+        };
       };
     };
 }
