5886de775bcac66c0fd86f36d93dc253b0b88997
Conor Knowles <dev@conorknowles.com>
2026-04-03 18:19:23 +0100

    fix bug in intgpu

--- modules/nixos/intgpu.nix
+++ modules/nixos/intgpu.nix
@@ -1,15 +1,16 @@
-{ pkgs, ... }:
 {
-  flake.modules.nixos.intgpu = {
-    hardware.graphics = {
-      enable = true;
-      extraPackages = with pkgs; [
-        intel-media-driver
-        vaapiVdpau
-        libvdpau-va-gl
-      ];
-    };
+  flake.modules.nixos.intgpu =
+    { pkgs, ... }:
+    {
+      hardware.graphics = {
+        enable = true;
+        extraPackages = with pkgs; [
+          intel-media-driver
+          vaapiVdpau
+          libvdpau-va-gl
+        ];
+      };
 
-    environment.sessionVariables.LIBVA_DRIVER_NAME = "iHD";
-  };
+      environment.sessionVariables.LIBVA_DRIVER_NAME = "iHD";
+    };
 }
