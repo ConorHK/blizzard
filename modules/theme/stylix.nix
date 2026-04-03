5f00da41eb651bdf15a9db7e81645a2cbf109d4e
Conor Knowles <dev@conorknowles.com>
2026-04-03 03:35:07 +0100

    Silence gtk4.theme warning by forwarding gtk.theme explicitly

--- modules/theme/stylix.nix
+++ modules/theme/stylix.nix
@@ -8,8 +8,11 @@
     };
   };
 
-  flake.modules.homeManager.theme = {
-    imports = [ inputs.stylix.homeModules.stylix ];
-    stylix.enable = true;
-  };
+  flake.modules.homeManager.theme =
+    { config, ... }:
+    {
+      imports = [ inputs.stylix.homeModules.stylix ];
+      stylix.enable = true;
+      gtk.gtk4.theme = config.gtk.theme;
+    };
 }
