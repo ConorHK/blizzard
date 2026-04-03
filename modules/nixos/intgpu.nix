bf76695f29f2450dc9ec9f77162742ba371f31c0
Conor Knowles <dev@conorknowles.com>
2026-04-03 18:21:27 +0100

    Replace deprecated package name

--- modules/nixos/intgpu.nix
+++ modules/nixos/intgpu.nix
@@ -6,7 +6,7 @@
         enable = true;
         extraPackages = with pkgs; [
           intel-media-driver
-          vaapiVdpau
+          libva-vdpau-driver
           libvdpau-va-gl
         ];
       };
