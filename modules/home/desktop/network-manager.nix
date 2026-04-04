c3055b602875004ccceb4eba73d11a25070c6b2d
Conor Knowles <dev@conorknowles.com>
2026-04-04 15:59:25 +0100

    Change network util

--- modules/home/desktop/network-manager.nix
+++ modules/home/desktop/network-manager.nix
@@ -2,16 +2,7 @@
   flake.modules.homeManager.network-manager =
     { pkgs, ... }:
     {
-      home.packages = [
-        pkgs.networkmanagerapplet
-      ];
-      wayland.windowManager.hyprland.settings = {
-        windowrule = [
-          "float on, match:class nm-connection-editor"
-          "center on, match:class nm-connection-editor"
-        ];
-      };
       programs.waybar.settings.main.network.on-click =
-        "${pkgs.networkmanagerapplet}/bin/nm-connection-editor";
+        "${pkgs.alacritty}/bin/alacritty --class alacritty-popup -e ${pkgs.networkmanager}/bin/nmtui-connect";
     };
 }
