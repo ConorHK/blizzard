19d5155dd716966dc22c2601fedb15a3647adede
Conor Knowles <dev@conorknowles.com>
2026-05-17 17:36:06 +0100

    Fix statix

--- modules/flake/checks.nix
+++ modules/flake/checks.nix
@@ -23,19 +23,36 @@
         yamllint.enable = true;
         ripsecrets.enable = true;
         shellcheck.enable = true;
-        statix = {
-          enable = true;
-          # statix can't parse pipe operators or flake input repeated-key patterns
-          settings.ignore = [
-            "flake.nix"
-            "templates/*"
-            "modules/home/core/cli/ssh.nix"
-            "modules/home/desktop/media/gfx.nix"
-            "modules/nixos/core/network/network-manager.nix"
-            "modules/nixos/desktop/android.nix"
-            "modules/nixos/desktop/wayland/default.nix"
-          ];
-        };
+        statix =
+          let
+            # statix can't parse pipe operators or flake input repeated-key patterns
+            ignored = [
+              "flake.nix"
+              "templates/*"
+              "modules/home/core/cli/ssh.nix"
+              "modules/home/desktop/media/gfx.nix"
+              "modules/nixos/core/network/network-manager.nix"
+              "modules/nixos/desktop/android.nix"
+              "modules/nixos/desktop/wayland/default.nix"
+            ];
+            configDir = pkgs.runCommand "statix-config" { } ''
+              mkdir -p $out
+              cat > $out/statix.toml <<EOF
+              disabled = []
+              nix_version = "2_4"
+              ignore = [${pkgs.lib.concatMapStringsSep ", " (p: ''"${p}"'') ignored}]
+              EOF
+            '';
+            wrapper = pkgs.writeShellScript "statix-check" ''
+              exec ${pkgs.lib.getExe pkgs.statix} check --config ${configDir}/statix.toml
+            '';
+          in
+          {
+            enable = true;
+            entry = toString wrapper;
+            pass_filenames = false;
+            files = "\\.nix$";
+          };
         treefmt.enable = true;
       };
     };
