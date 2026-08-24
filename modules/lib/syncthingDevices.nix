280b22d1495833551fa81ba2bcff0d94c62a1a71
Conor Knowles <dev@conorknowles.com>
2026-08-24 21:10:08 +0100

    feat(syncthing): add banshee to the mesh

--- modules/lib/syncthingDevices.nix
+++ modules/lib/syncthingDevices.nix
@@ -3,6 +3,7 @@
   # Every peer must list every other peer.
   flake.lib.syncthingDevices = {
     abhartach = "6UC67WT-CMLMJIP-JA6Z2H2-2H2ICCF-N7VRJBY-4XOMVIO-A6E7TN4-JVSW4A4";
+    banshee = "7XZZBRK-OFL37ZO-UAFZTAE-VAJUQR2-VPL5ZT4-GUTZDBE-KNYLI5U-2VOWGAP";
     dullahan = "LAIGIVW-EIN4X7E-NTWJJ7D-XE6UBDQ-LXALLB6-UR77FXM-JZJMQJ7-5TKPNAX";
     # Written by the `syncthing-key` agenix generator.
     leprechaun = lib.removeSuffix "\n" (builtins.readFile ../nixos/server/syncthing/device-id);
