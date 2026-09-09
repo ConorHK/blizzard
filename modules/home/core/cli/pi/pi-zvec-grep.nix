2f4cc2c420ea3f643488a435a31c62df08ccd566
Conor Knowles <dev@conorknowles.com>
2026-09-09 23:33:54 +0100

    Move to vended zvec-grep plugin

--- modules/home/core/cli/pi/pi-zvec-grep.nix
+++ modules/home/core/cli/pi/pi-zvec-grep.nix
@@ -2,10 +2,28 @@
 {
   flake.modules.homeManager.pi-zvec-grep =
     { pkgs, ... }:
+    let
+      # No runtime deps; only pi-bundled peer deps,
+      # so the npm tarball needs no npm build.
+      pi-zvec-grep = pkgs.stdenvNoCC.mkDerivation rec {
+        pname = "pi-zvec-grep";
+        version = "0.3.1";
+        src = pkgs.fetchurl {
+          url = "https://registry.npmjs.org/@luminascale/pi-zvec-grep/-/pi-zvec-grep-${version}.tgz";
+          hash = "sha256-V4XBaqnxTc9y+8CMEW8cbBzrmgf8BejnyTceS4nRPSg=";
+        };
+        dontConfigure = true;
+        dontBuild = true;
+        installPhase = ''
+          runHook preInstall
+          mkdir -p $out
+          tar xzf $src -C $out --strip-components=1
+          runHook postInstall
+        '';
+      };
+    in
     {
-      # Directory extension: index.ts + src/, vendored from
-      # github.com/MikkelKappelPersson/pi-zvec-grep v0.3.1.
-      programs.pi.extensionDirs.zvec-grep = ./zvec-grep;
+      programs.pi.extensionDirs.zvec-grep = pi-zvec-grep;
 
       # zg must be on PATH; the extension shells out to it.
       home.packages = [
