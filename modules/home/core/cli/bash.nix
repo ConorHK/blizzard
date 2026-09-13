22cf8980f28d473ea6851c2ac0dd61cb92101688
Conor Knowles <dev@conorknowles.com>
2026-09-13 22:58:42 +0100

    bash: source session vars interactively

--- modules/home/core/cli/bash.nix
+++ modules/home/core/cli/bash.nix
@@ -1,3 +1,12 @@
 {
-  flake.modules.homeManager.core.programs.bash.enable = true;
+  flake.modules.homeManager.core =
+    { config, ... }:
+    {
+      programs.bash = {
+        enable = true;
+        # Login shells get vars via ~/.profile; interactive-only shells
+        # (zellij panes) read this instead.
+        initExtra = ''. "${config.home.profileDirectory}/etc/profile.d/hm-session-vars.sh"'';
+      };
+    };
 }
