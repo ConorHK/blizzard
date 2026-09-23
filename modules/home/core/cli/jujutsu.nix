85f3625bcddfc5ecbae8af69f9b3f09fc8e32305
Conor Knowles <knoconor@amazon.com>
2026-09-23 14:27:45 +0000

    Add nice jj log

--- modules/home/core/cli/jujutsu.nix
+++ modules/home/core/cli/jujutsu.nix
@@ -1,5 +1,33 @@
+let
+  markers = builtins.fromJSON ''
+    { "empty": "\u25a2", "noDescription": "\u2190" }
+  '';
+in
 {
   flake.modules.homeManager.jujutsu = {
-    programs.jujutsu.enable = true;
+    programs.jujutsu = {
+      enable = true;
+      settings = {
+        templates.log = "simple_log";
+        template-aliases = {
+          empty_commit_marker = "label('empty', '${markers.empty}')";
+          simple_log = ''
+            separate(" ",
+              pad_end(
+                4,
+                separate("/",
+                  self.change_id().shortest(),
+                  if(self.divergent() || self.hidden(), self.change_offset())
+                )
+              ),
+              if(self.immutable(), "L ", "  ") ++ " ",
+              if(self.empty(), empty_commit_marker),
+              if(self.description(), self.description().first_line(), if(self.empty(), "", label('no_description', '${markers.noDescription}'))),
+              self.bookmarks()
+            )
+          '';
+        };
+      };
+    };
   };
 }
